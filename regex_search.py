125e67cc96f78128598965c2b0defa9612e9ffb3
szeitlin <szeitlin@users.noreply.github.com>
2014-03-20 11:06:23 -0700

    Create regex_search.py
    very simple example for practice.

---
+++ regex_search.py
@@ -0,0 +1,20 @@
+__author__ = 'szeitlin'
+
+#from pythonchallenge
+
+#according to the hints, it sounds like they want you to write a regular expression and look for
+#a three-letter string (consecutive characters that are all letters)
+#not actually what they want, and not at all what I thought from the original description!
+
+import re
+
+newstring = ""
+
+with open("ocr.txt") as myfile:
+    for line in myfile:
+        newstring += line.strip() #not sure if strip was actually required
+
+    p = re.compile("[a-z]+") #[a-z] should give consecutive letters a-z
+    q = p.findall(newstring)
+    if q != []:
+        print q
